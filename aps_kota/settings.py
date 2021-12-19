3e5b456dc9007edb6ef2932449cb68661dc54781
VishalGoswami <vishal0711goswami@gmail.com>
2021-12-19 17:05:17 +0530

    S

--- aps_kota/settings.py
+++ aps_kota/settings.py
@@ -132,9 +132,11 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR, "public/static")
-}
+# STATICFILES_DIR = {
+#     os.path.join(BASE_DIR, "public/static")
+# }
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "public/static")
 MEDIA_URL = "/media/"
--- aps_kota/urls.py
+++ aps_kota/urls.py
@@ -4,8 +4,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +12,9 @@ urlpatterns = [
     path("froala_editor/", include('froala_editor.urls')),
     # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL, 
                             document_root=settings.MEDIA_ROOT)
