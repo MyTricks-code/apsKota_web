efa3b5792c8d58d54767fa851987665fe3dc37b9
VishalGoswami <vishal0711goswami@gmail.com>
2022-02-05 21:57:20 +0530

    Update settings.py

--- aps_kota/settings.py
+++ aps_kota/settings.py
@@ -135,14 +135,12 @@ USE_TZ = True
 #Added manually
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
 
 
 # STATICFILES_DIR = {
 #     os.path.join(BASE_DIR, "public/static")
 # }
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "public/static")
 MEDIA_URL = "/media/"
